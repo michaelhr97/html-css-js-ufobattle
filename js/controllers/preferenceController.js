02f3f1412308a8987426e1ec4fbdba94cb539ce4
michael <michaelhernandezrala@gmail.com>
2023-11-12 18:16:51 +0100

    CSS refactor and preference page complete

---
+++ js/controllers/preferenceController.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  const TOTAL_NUMBER_UFO = 6;
+  const TOTAL_TIME = 60;
+
+  let form = document.getElementById('preference__page--form');
+  let totalufo = document.getElementById('totalufo');
+  let time = document.getElementById('time');
+
+  totalufo.value = localStorage.getItem('totalufo') || TOTAL_NUMBER_UFO;
+  time.value = localStorage.getItem('time') || TOTAL_TIME;
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('totalufo', totalufo.value);
+    localStorage.setItem('time', time.value);
+  });
+});
