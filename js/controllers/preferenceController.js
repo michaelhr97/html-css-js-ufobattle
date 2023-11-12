39bb48c38c287f2e0ef6cccf426b28b49c68ec12
michael <michaelhernandezrala@gmail.com>
2023-11-12 18:37:17 +0100

    Some fixes in preference page

--- js/controllers/preferenceController.js
+++ js/controllers/preferenceController.js
@@ -1,12 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
-  const TOTAL_NUMBER_UFO = 6;
+  const TOTAL_NUMBER_UFO = 1;
   const TOTAL_TIME = 60;
 
   let form = document.getElementById('preference__page--form');
   let totalufo = document.getElementById('totalufo');
   let time = document.getElementById('time');
+
+  console.log(time.value);
 
   totalufo.value = localStorage.getItem('totalufo') || TOTAL_NUMBER_UFO;
   time.value = localStorage.getItem('time') || TOTAL_TIME;
